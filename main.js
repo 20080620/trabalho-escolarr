bb4968a2ddd6a26e5e9cba3a62bdc478be99293a
20080620 <yuri.peretiatko@escola.pr.gov.br>
2026-06-24 08:27:51 -0300

    .

--- main.js
+++ main.js
@@ -16,11 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-12-18T00:00:00")
-const tempoObjetivo2 = new Date("2026-12-18T00:00:00")
-const tempoObjetivo3 = new Date("2026-12-18T00:00:00")
-const tempoObjetivo4 = new Date("2026-12-18T00:00:00")
+const tempoObjetivo2 = new Date("2027-12-08T00:00:00")
+const tempoObjetivo3 = new Date("2032-12-10T00:00:00")
+const tempoObjetivo4 = new Date("2038-12-01T00:00:00")
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
 
